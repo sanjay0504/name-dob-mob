8f673256fec37efc9cbdffbfb3bfcbffe717cba3
Sanjay Errabelly <97969474+sanjay0504@users.noreply.github.com>
2022-01-29 12:10:55 +0530

    Add files via upload

---
+++ name,dob,num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+{
+    printf("Name: Sanjay Errabelly\n");
+    printf("DOB: 17/05/2004\n");
+    printf("Mobile: +91-9391865925\n");
+
+    return(0);
+
+}
